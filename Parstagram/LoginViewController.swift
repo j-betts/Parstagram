39968b1dbc3b7bd8dd227e958b4cd1af04a0bbc9
Justin Betts <bett9421@m506-118-26437.local>
2020-02-26 21:04:38 -0800

    42 errors with alamofire

--- Parstagram/LoginViewController.swift
+++ Parstagram/LoginViewController.swift
@@ -7,16 +7,37 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    @IBAction func onSignIn(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground { (success, error) in
+            if error != nil {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+    }
     
-
+    @IBAction func onSignUp(_ sender: Any) {
+    }
+    
     /*
     // MARK: - Navigation
 
