374440f9bbfe94854f209cd51db107369fbd2cee
Justin Betts <jbetts@csumb.edu>
2020-03-03 22:42:42 -0800

    I can't get any simulators to actually run this project. Not sure if it even works.

--- Parstagram/LoginViewController.swift
+++ Parstagram/LoginViewController.swift
@@ -22,6 +22,20 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     @IBAction func onSignIn(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password)
+            { (user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
         let user = PFUser()
         user.username = usernameField.text
         user.password = passwordField.text
@@ -32,11 +46,7 @@ class LoginViewController: UIViewController {
             } else {
                 print("Error: \(error?.localizedDescription)")
             }
-        }
-    }
-    
-    @IBAction func onSignUp(_ sender: Any) {
-    }
+        }    }
     
     /*
     // MARK: - Navigation
